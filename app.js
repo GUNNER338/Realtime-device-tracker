346d1e46ce390201d2724964ea9eb16086b501aa
Gurpartap Singh <gsranu338@gmail.com>
2025-07-26 00:19:08 +0530

    worked on index.ejs

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express")
 const app = express()
 const http = require("http")
+const path = require("path")
 
-const socketio = require(socketio)
+
+const socketio = require("socket.io")
 const server=http.createServer(app)
 
 const  io = socketio(server)
 
-
-app.get("/", function (req, res){
-    res.send("hey")
+io.on("connection", function(socket){
+    console.log("connected")
 })
 
-app.listen(3000);
+app.set("view engine","ejs")
+app.set(express.static(path.join(__dirname,"public")))
+
+app.get("/", function (req, res){
+    res.render("index")
+})
+
+server.listen(3000);
---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", longitude, latitude);
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000
+    }
+  );
+}
