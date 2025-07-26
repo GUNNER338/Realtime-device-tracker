631a4fc4734d453c880e15cf4cb730f2556aa5ed
Gurpartap Singh <gsranu338@gmail.com>
2025-07-26 15:52:53 +0530

    worked on a real-time-location tracker

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-const express = require("express")
-const app = express()
-const http = require("http")
-const path = require("path")
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+
+const io = socketio(server);
+
+io.on("connection", function (socket) {
+  console.log("Client connected:", socket.id);
+
+  socket.on("send-location", function (longitude, latitude) {
+    console.log("Received location from", socket.id, longitude, latitude);
+
+    // Spread lat/lng into response
+    io.emit("receive-location", {
+      id: socket.id,
+      longitude: longitude,
+      latitude: latitude
+    });
+  });
+  socket.on("disconnect", function(){
+      io.emit("user-disconnected",socket.id)
+  })
+});
 
 
-const socketio = require("socket.io")
-const server=http.createServer(app)
 
-const  io = socketio(server)
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    console.log("connected")
-})
-
-app.set("view engine","ejs")
-app.set(express.static(path.join(__dirname,"public")))
-
-app.get("/", function (req, res){
-    res.render("index")
-})
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 server.listen(3000);
--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,49 @@ if (navigator.geolocation) {
     }
   );
 }
+
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Gurpartap",
+}).addTo(map);
+
+const markers = {}; // Map to store markers per device
+
+socket.on("receive-location", (data) => {
+  console.log("data", data);
+
+  const { id, latitude, longitude } = data;
+ 
+  if (
+    typeof latitude !== "number" ||
+    typeof longitude !== "number" ||
+    isNaN(latitude) ||
+    isNaN(longitude)
+  ) {
+    console.warn("Invalid coordinates received!", latitude, longitude);
+    return;
+  }
+
+  // Center map on this location
+  map.setView([latitude, longitude], 16);
+
+  if (markers[id]) {
+    // Update marker position
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    // Create new marker and add to map
+    markers[id] = L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup(`Device ID: ${id}`)
+      .openPopup();
+  }
+});
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id])
+        delete markers[id];
+    }
+})
